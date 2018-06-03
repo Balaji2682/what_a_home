1b042eecc9d0abba7f7060d77e7bf66910562560
Balaji <17160351+Balaji2682@users.noreply.github.com>
2018-06-03 08:54:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "May 29, 2018 18:03:26",
+	date: "May 29, 2019 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
